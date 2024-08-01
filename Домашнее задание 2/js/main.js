512166d3d8b6746290ed197e571f06faf0d9aa9b
Thinker-number-one <grandmaster.denis@yandex.ru>
2024-08-02 01:06:34 +0500

    Add files via upload

---
+++ Домашнее задание 2/js/main.js
@@ -0,0 +1,14 @@
+console.log(11); // Я родился в ноябре
+
+console.log('Добро' + ' ' + 'пожаловать' + ' ' + 'на курс' + ' ' + 'Денис');
+
+// alert('02.08.2024');
+
+document.getElementById('one').innerHTML = 'Hello World';
+
+console.log(typeof 10);
+console.log(typeof 'cat');
+console.log(typeof 10n);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
